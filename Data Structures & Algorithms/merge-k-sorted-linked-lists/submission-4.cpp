4b6aa80997d8787ac89b2d6a9fd24b806380e6cd
notion <155753021+git-notion@users.noreply.github.com>
2026-08-01 15:08:59 +0530

    Add: merge-k-sorted-linked-lists - submission-4

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-4.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ListNode* mergeList(ListNode* l1, ListNode *l2){
+    ListNode dummy;
+    ListNode* tail=&dummy;
+    while(l1&&l2){
+        if(l1->val<l2->val){
+            tail->next=l1;
+            l1=l1->next;
+        }
+        else{
+            tail->next=l2;
+            l2=l2->next;
+        }
+        tail=tail->next;
+    }
+    if(l1){
+        tail->next=l1;
+    }
+    if(l2){
+        tail->next=l2;
+    }
+    return dummy.next;
+    
+}
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.empty()){
+            return NULL;
+        }
+        while(lists.size()>1){
+            vector<ListNode*>mergedLists;
+            for(int i=0;i<lists.size();i+=2){
+                ListNode* l1=lists[i],*l2=(i+1)<lists.size()?lists[i+1]:NULL;
+                mergedLists.push_back(mergeList(l1,l2));
+            }
+            lists=mergedLists;
+        }
+        return lists[0];
+    }
+};
