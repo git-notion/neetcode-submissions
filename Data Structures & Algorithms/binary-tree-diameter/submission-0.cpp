9a646fc96011a86447f9b8119ae790378ff31c8a
notion <155753021+git-notion@users.noreply.github.com>
2026-08-04 23:01:03 +0530

    Add: binary-tree-diameter - submission-0

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-0.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int ans=0;
+    int diameterOfBinaryTree(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+private:
+    int dfs(TreeNode* root){
+        if(root==nullptr) return 0;
+        int l=dfs(root->left);
+        int r=dfs(root->right);
+        ans=max(ans,l+r);
+        return 1+max(l,r);
+    }
+};
