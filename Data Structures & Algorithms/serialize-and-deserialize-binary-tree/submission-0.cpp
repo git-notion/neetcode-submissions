5d972e3881c80784e94200b00274252def7af6c5
notion <155753021+git-notion@users.noreply.github.com>
2026-08-10 13:39:17 +0530

    Add: serialize-and-deserialize-binary-tree - submission-0

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-0.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Codec {
+public:
+    string tree="";
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        vector<string>res;
+        dfs(root,res);
+        return join(res,",");
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        vector<string> vals=split(data,',');
+        int i=0;
+        return dfs2(vals,i);
+        
+    }
+private:
+    void dfs(TreeNode* root,vector<string>&res){
+        if(!root){
+            res.push_back("N");
+            return;
+        }
+        res.push_back(to_string(root->val));
+        dfs(root->left,res);
+        dfs(root->right,res);
+    }
+    TreeNode* dfs2(vector<string>& vals, int& i) {
+        if(vals[i]=="N"){
+            i++;
+            return NULL;
+        }
+        TreeNode* node=new TreeNode(stoi(vals[i]));
+        i++;
+        node->left=dfs2(vals,i);
+        node->right=dfs2(vals,i);
+        return node;
+    }
+    vector<string> split (const string &s, char delim){
+        vector<string> elems;
+        stringstream ss(s);
+        string item;
+        while(getline(ss,item,delim)){
+            elems.push_back(item);
+        }
+        return elems;
+    }
+    string join(const vector<string> &v, const string &delim){
+        ostringstream s;
+        for(const auto &i: v){
+            if(&i!=&v[0])   s<<delim;
+            s<<i;
+        }
+        return s.str();
+    }
+};
